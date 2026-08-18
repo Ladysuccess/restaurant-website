91c77d7b7c0cdbfb88f83aa50c22701dea5d499f
Ladysuccess <faithpafiesimama@gmail.com>
2026-08-19 00:40:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let restaurantName = "Faith's Restaurant";
 function orderNow() {
     alert("Welcome to " + restaurantName + "! Your order is being prepared.");
 }
+
+let age = 25;
+
+If (age>=) {
+    console.log("You can place an order.");
+} else {
+    console.log("You must be 18 or older.");
+}
